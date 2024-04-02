34757b0c86e43a8b343d20a58f3cef3502e18ee1
Suvakanta8 <90302272+Suvakanta8@users.noreply.github.com>
2024-04-02 18:58:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ import streamlit as st
 from PIL import Image
 from contentgen_app import content_main
 from summary_app import summary_main
-col1, col2, col3, col4 = st.columns([1,1,6,1])
 
-with col1:
- st.write("")
-
-with col2:
-   st.write("")
+def main():
+   col1, col2, col3, col4 = st.columns([1,1,6,1])
    
-with col3:
-  st.title("Bull Trend 📈")
-
-with col4:
- st.write("")
-
-# Radio buttons for selecting the section
-selected_section = st.sidebar.radio("Select Section", ["Generate Blog","Summary"])
-
-if selected_section == "Generate Blog":
-    content_main()
-elif selected_section == "Summary":
-    summary_main()
+   with col1:
+    st.write("")
+   
+   with col2:
+      st.write("")
+      
+   with col3:
+     st.title("Bull Trend 📈")
+   
+   with col4:
+    st.write("")
+   
+   # Radio buttons for selecting the section
+   selected_section = st.sidebar.radio("Select Section", ["Generate Blog","Summary"])
+   
+   if selected_section == "Generate Blog":
+       content_main()
+   elif selected_section == "Summary":
+       summary_main()
+    
+if __name__ == "__main__":
+    main()
